c67e37cdb715d58b0acc61118e185ae74067a5bd
Rakesh Sagar <rakeshsagar64@gmail.com>
2020-09-13 10:38:12 +0530

    weekend java

---
+++ app.js
@@ -0,0 +1,84 @@
+//  variables
+// let, const
+
+// number 12, 23.45
+// String 'sdsd'
+// array
+// object
+// undefined
+// boolean
+
+// const arr = ['one', 'two', 'three'];
+
+// console.log(arr);
+
+// const obj = {
+//     name: 'User',
+//     place: {
+//         city: 'Bangalore',
+//         state: 'Karnataka'
+//     },
+//     hobbies: ['tv', 'stuff']
+// };
+
+// const arrayOfUsers = [ {
+//     name: 'User',
+//     place: {
+//         city: 'Bangalore',
+//         state: 'Karnataka'
+//     },
+//     hobbies: ['tv', 'stuff']
+// }, {name: 'Ram2'}, {name: 'Ram3'}];
+
+
+// // console.log(123>34);
+
+// let bq;
+// arr.push('Hello');
+// console.log(arr);
+// console.log(bq);
+
+// function myFunction(){
+//     console.log("Hello world");
+// }
+
+// myFunction();
+
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
+
+// const anotherFunction = function(farg){
+//     farg();
+// };
+
+// anotherFunction(myFunction);
+
+// // arrow function
+// const arrow = (a) => {
+//     console.log(a);
+//     return a+10;
+// };
+
+// const shortArrow = () => "Hello";
+
+// console.log(shortArrow());
+
+// // short hand syntax ---> only functions with one line -->return stmt
+// const op = arrow(23);
+// console.log(op);
+
+const obj = {
+    name: 'User',
+    place: {
+        city: 'Bangalore',
+        state: 'Karnataka'
+    },
+    hobbies: ['tv', 'stuff']
+};
+
+console.log(obj);
+
+obj.age = 23;
+console.log(obj['hobbies']);
+
